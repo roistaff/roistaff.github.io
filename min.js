6240bce6aae92ca18596a0a8a28b18dac7a24a9f
roistaff <roistaff1983@gmail.com>
2023-05-19 17:43:30 +0900

    I'm making this more cool

--- min.js
+++ min.js
@@ -1,3 +1,8 @@
+function switchThemeMode(){
+    let input = document.getElementById('switch');
+}
+
+
 function scroll_effect() {var element = document.getElementsByClassName('scroll-up');if(!element) return;var scrollY = window.pageYOffset;var windowH = window.innerHeight;
 if(window.matchMedia && window.matchMedia('(max-device-width: 740px)').matches) {wt = windowH * 0.6;} else {wt = windowH * 0.4;}
 var showTiming = wt;for(var i = 0; i < element.length; i++) {var elemClientRect = element[i].getBoundingClientRect();var elemY = scrollY + elemClientRect.top;if(scrollY > elemY - windowH + showTiming){element[i].classList.add('is-show');}}}
