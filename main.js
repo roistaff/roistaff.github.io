ed862c49fb95f988526f031594ae4caba74fe6bb
roistaff <roistaff1983@gmail.com>
2023-05-20 15:33:24 +0900

    up seo

--- main.js
+++ main.js
@@ -1,40 +1,51 @@
-  function scroll_effect() {
-    var element = document.getElementsByClassName('scroll-up');
-    if(!element) return;
-                        
-    var scrollY = window.pageYOffset;
-    var windowH = window.innerHeight;
-  if (window.matchMedia && window.matchMedia('(max-device-width: 740px)').matches) {
-    wt = windowH * 0.6;
-  } else {
-    wt = windowH * 0.4;
-  }
-    var showTiming = wt; // 要素を表示するタイミング
-    for(var i = 0; i < element.length; i++) { 
-      var elemClientRect = element[i].getBoundingClientRect(); 
-      var elemY = scrollY + elemClientRect.top; 
-      if(scrollY > elemY - windowH + showTiming) {
-        element[i].classList.add('is-show');
-      }
-    }
-  }
-  window.addEventListener('scroll', scroll_effect);
+let input = document.getElementById('switch');
+
+function switchThemeMode() {
+	let label = document.getElementById('label');
+	let logo = document.getElementById('logo');
+	document.body.classList.toggle('dark-theme');
+	if (document.body.classList.value == 'dark-theme') {
+		logo.src = 'imgs/logo-dark.png';
+	} else {
+		logo.src = 'imgs/logo-light.png';
+	}
+}
+
+function scroll_effect() {
+	var element = document.getElementsByClassName('scroll-up');
+	if (!element) return;
+	var scrollY = window.pageYOffset;
+	var windowH = window.innerHeight;
+	if (window.matchMedia && window.matchMedia('(max-device-width: 740px)').matches) {
+		wt = windowH * 0.6;
+	} else {
+		wt = windowH * 0.4;
+	}
+	var showTiming = wt;
+	for (var i = 0; i < element.length; i++) {
+		var elemClientRect = element[i].getBoundingClientRect();
+		var elemY = scrollY + elemClientRect.top;
+		if (scrollY > elemY - windowH + showTiming) {
+			element[i].classList.add('is-show');
+		}
+	}
+}
+window.addEventListener('scroll', scroll_effect);
 async function getNews() {
-    const response = await fetch(
-      "news.json" 
-          );
-    const news = await response.json();
-    console.log(news);
-    return news;
-  }
-  async function InputNews(){
-    news = await getNews();
-    newslen = Object.keys(news).length;
-    let newsfeed = document.getElementById('newsfeed');
-    for (let i = 0; i < newslen; i++){
-      let child = document.createElement('li')
-      child.prepend(news[i].date +"　" + news[i].todo)
-      newsfeed.append(child);
-    }
-  }
+	const response = await fetch("news.json");
+	const news = await response.json();
+	console.log(news);
+	return news;
+}
+async function InputNews() {
+	news = await getNews();
+	newslen = Object.keys(news).length;
+	let newsfeed = document.getElementById('newsfeed');
+	for (let i = 0; i < newslen; i++) {
+		let child = document.createElement('li');
+		child.prepend(news[i].date + "　" + news[i].todo);
+		newsfeed.append(child);
+	}
+}
 InputNews()
+input.addEventListener('change', switchThemeMode);
--- min.js
+++ min.js
@@ -1,18 +1,5 @@
 let input = document.getElementById('switch');
-function getDarkmode(){
-    let dark = window.matchMedia('(prefers-color-scheme: dark)').matches ;
-    return dark;
-}
-function switchThemeMode(){
-    let label = document.getElementById('label');
-    let logo = document.getElementById('logo');
-    document.body.classList.toggle('dark-theme');
-    if (document.body.classList.value == 'dark-theme'){
-    logo.src = 'imgs/logo-dark.png';
-}else{
-    logo.src = 'imgs/logo-light.png'
-}
-}
+function switchThemeMode(){let label = document.getElementById('label');let logo = document.getElementById('logo');document.body.classList.toggle('dark-theme');if (document.body.classList.value == 'dark-theme'){logo.src = 'imgs/logo-dark.png';}else{logo.src = 'imgs/logo-light.png';}}
 function scroll_effect() {var element = document.getElementsByClassName('scroll-up');if(!element) return;var scrollY = window.pageYOffset;var windowH = window.innerHeight;
 if(window.matchMedia && window.matchMedia('(max-device-width: 740px)').matches) {wt = windowH * 0.6;} else {wt = windowH * 0.4;}
 var showTiming = wt;for(var i = 0; i < element.length; i++) {var elemClientRect = element[i].getBoundingClientRect();var elemY = scrollY + elemClientRect.top;if(scrollY > elemY - windowH + showTiming){element[i].classList.add('is-show');}}}
